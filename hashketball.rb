b4eb62950db198dab9612060ee1804c9d848ea50
Alex Merecka <merecka@gmail.com>
2019-03-25 20:51:37 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,15 +1,28 @@
 def game_hash
   my_hash = {
     :home => {
-      :team_name => "Brooklyn Nets"
-      :colors => ["Black", "White"]
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
       :players =>
     }
 
     :away => {
-      :team_name => "Charlotte Hornets"
-      :colors => ["Turquoise", "Purple"]
-      :players =>
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => {
+        players_hash = {
+          "Alan Anderson" => {
+            :number => 0,
+            :shoe => 16,
+            :points => 22,
+            :rebounds => 12,
+            :assists => 12,
+            :steals => 3,
+            :blocks => 1,
+            :slam_dunks => 1
+          }
+        }
+      }
     }
 
   }
