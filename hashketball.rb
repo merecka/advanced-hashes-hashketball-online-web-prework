250e6317f5b39b867646c66102f9d9a3b0e9b370
Alex Merecka <merecka@gmail.com>
2019-03-25 20:26:20 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,16 @@
-# Write your code here!
+def game_hash
+  my_hash = {
+    :home => {
+      :team_name => "Brooklyn Nets"
+      :colors => ["Black", "White"]
+      :players =>
+    }
 
+    :away => {
+      :team_name => "Charlotte Hornets"
+      :colors => ["Turquoise", "Purple"]
+      :players =>
+    }
 
-
-
-
-
-
-
-
-
+  }
+end
