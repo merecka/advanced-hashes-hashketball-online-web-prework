7c5c0206f5cf2915e71ddb1460ea08a1e5305a74
Alex Merecka <merecka@gmail.com>
2019-03-27 02:36:33 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -253,31 +253,41 @@ def big_shoe_rebounds
 end
 
 def most_points_scored
+  player_hash = {}
   game_hash.each do |location, team_data| #location returns away, team_data returns hash
   #  binding.pry
+
     team_data.each do |attribute, data|
   #  binding.pry
+
       if attribute == :players
-        data.each do |player_name, categories|
-          new_hash = {player_name => categories[:points]}
-           return new_hash.key(new_hash.values.max)
-        end
+
+        player_hash{attribute => data}
+        binding.pry
+        #data.each do |player_name, categories|
+          #new_hash = {player_name => categories[:points]}
+          #binding.pry
+          # return new_hash.key(new_hash.values.max)
+        #end
       end
     end
+
   end
 end
 
-def winning_team
-  game_hash.each do |location, team_data| #location returns away, team_data returns hash
-    binding.pry
-    team_data.each do |attribute, data|
-  #  binding.pry
-      if attribute == :players
-        data.each do |player_name, categories|
-          new_hash = {player_name => categories[:points]}
-           return new_hash.key(new_hash.values.max)
-        end
-      end
-    end
-  end
-end
+# def winning_team
+#   game_hash.each do |location, team_data| #location returns away, team_data returns hash
+#   #  binding.pry
+#     team_data.each do |attribute, data|
+#   #  binding.pry
+#       if attribute == :players
+#   #      binding.pry
+#         data.each do |player_name, categories|
+#           new_hash = {player_name => categories[:points]}
+#       #    binding.pry
+#            return new_hash.key(new_hash.values.max)
+#         end
+#       end
+#     end
+#   end
+# end
